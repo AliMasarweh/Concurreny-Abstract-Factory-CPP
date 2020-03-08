a6dcc214e5bff671a95e3e1f5efcd293e02bc778
Ali Masarwe <alis.masarwe@gmail.com>
2020-03-08 04:28:31 -0700

    Basic interface of the project

---
+++ mutex.h
@@ -0,0 +1,15 @@
+//
+// Created by ali-masa on 3/8/20.
+//
+
+#ifndef PLATFORMINDEPENTCONCURENCY_MUTEX_H
+#define PLATFORMINDEPENTCONCURENCY_MUTEX_H
+
+class Mutex
+{
+public:
+    virtual void lock() = 0;
+    virtual void unlock() = 0;
+};
+
+#endif //PLATFORMINDEPENTCONCURENCY_MUTEX_H
---
+++ thread.h
@@ -0,0 +1,15 @@
+//
+// Created by ali-masa on 3/8/20.
+//
+
+#ifndef PLATFORMINDEPENTCONCURENCY_THREAD_H
+#define PLATFORMINDEPENTCONCURENCY_THREAD_H
+
+class Thread
+{
+public:
+    virtual void run() = 0;
+    virtual void* join() = 0;
+};
+
+#endif //PLATFORMINDEPENTCONCURENCY_THREAD_H
